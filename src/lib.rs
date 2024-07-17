b2471cafe3d670cfbf3f4f51b4b0d8bdec826064
Kirill Chibisov <contact@kchibisov.com>
2024-07-17 13:17:18 +0300

    chore: remove deprecated feature cargo-clippy

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 #![deny(missing_debug_implementations)]
 #![deny(missing_docs)]
 #![forbid(unsafe_code)]
-#![cfg_attr(feature = "cargo-clippy", deny(warnings))]
+#![cfg_attr(clippy, deny(warnings))]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 //! The cross platform cursor icon type.
